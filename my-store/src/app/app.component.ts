52f543a299a459de9b8d76511292c2e85a925459
laucavv <laucavv23@gmail.com>
2021-10-29 14:43:14 -0500

    add list product

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './products-model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  listProduct: Product[] = [
+    {
+      name: "bear-shirt",
+      img: "./assets/bear-shirt.jpg",
+      price:23
+    },
+    {
+      name: "basic-shirt",
+      img: "./assets/basic-shirt.png",
+      price:22
+    },
+    {
+      name: "cloud-shirt",
+      img: "./assets/cloud-shirt.jpg",
+      price:32
+    },
+    {
+      name: "coffee-shirt",
+      img: "./assets/coffee-shirt.jpg",
+      price:29
+    },
+    {
+      name: "dinosaur-shirt",
+      img: "./assets/dinosaur-shirt.jpg",
+      price:15
+    },
+    {
+      name: "map-shirt",
+      img: "./assets/map-shirt.jpg",
+      price:15
+    },
+    {
+      name: "mice-shirt",
+      img: "./assets/mice-shirt.jpg",
+      price:23
+    },
+    {
+      name: "plant-shirt",
+      img: "./assets/plant-shirt.jpg",
+      price:23
+    }
+  ]
 }
